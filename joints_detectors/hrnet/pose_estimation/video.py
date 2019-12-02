22c498df577a53ea728ca04b171aed87b9acf45f
zh-plus <11610127@mail.sustech.edu.cn>
2019-12-03 01:15:45 +0800

    Remove debug info

--- joints_detectors/hrnet/pose_estimation/video.py
+++ joints_detectors/hrnet/pose_estimation/video.py
@@ -136,8 +136,8 @@ def generate_kpts(video_name, smooth=False):
             preds, maxvals = get_final_preds(
                 cfg, output.clone().cpu().numpy(), np.asarray(center), np.asarray(scale))
 
-            if len(preds) != 1:
-                print('here')
+            # if len(preds) != 1:
+            #     print('here')
 
         if smooth:
             # smooth and fine-tune coordinates
