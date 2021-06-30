31470f69d339e39bfa81c037789a0fb0634431d4
Zexi <zexi@zexi.me>
2021-07-01 01:07:36 +0800

    Fix transparency

--- joints_detectors/Alphapose/fn.py
+++ joints_detectors/Alphapose/fn.py
@@ -194,7 +194,7 @@ def vis_frame(frame, im_res, format='coco'):
             bg = img.copy()
             cv2.circle(bg, (int(cor_x / 2), int(cor_y / 2)), 2, p_color[n], -1)
             # Now create a mask of logo and create its inverse mask also
-            transparency = max(0, min(1, kp_scores[n]))
+            transparency = max(0, min(1, kp_scores[n])).item()
             img = cv2.addWeighted(bg, transparency, img, 1 - transparency, 0)
 
         # Draw proposal score on the head
@@ -219,7 +219,7 @@ def vis_frame(frame, im_res, format='coco'):
                 polygon = cv2.ellipse2Poly((int(mX), int(mY)), (int(length / 2), int(stickwidth)), int(angle), 0, 360, 1)
                 cv2.fillConvexPoly(bg, polygon, line_color[i])
                 # cv2.line(bg, start_xy, end_xy, line_color[i], (2 * (kp_scores[start_p] + kp_scores[end_p])) + 1)
-                transparency = max(0, min(1, 0.5 * (kp_scores[start_p] + kp_scores[end_p])))
+                transparency = max(0, min(1, 0.5 * (kp_scores[start_p] + kp_scores[end_p]))).item()
                 img = cv2.addWeighted(bg, transparency, img, 1 - transparency, 0)
     img = cv2.resize(img, (width, height), interpolation=cv2.INTER_CUBIC)
     return img
